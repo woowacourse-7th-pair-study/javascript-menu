53b05b8f0db1c455ab3068c9d4529d9f9c3fd05d
ohgus <123801385+ohgus@users.noreply.github.com>
2024-11-29 16:38:40 +0900

    feat: 결과 출력 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -10,6 +10,18 @@ const OutputView = {
   printEnd() {
     Console.print(OUTPUT_MESSAGE.end);
   },
+
+  printResult(coachArray, categories) {
+    Console.print('\n메뉴 추천 결과입니다.');
+    Console.print('[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]');
+    Console.print(`[ 카테고리 | ${categories.join(' | ')} ]`);
+    coachArray.forEach((coach) => {
+      const name = coach.getName();
+      const weeklyMenu = coach.getWeeklyMenu(categories);
+
+      Console.print(`[ ${name} | ${weeklyMenu.join(' | ')} ]`);
+    });
+  },
 };
 
 module.exports = OutputView;
