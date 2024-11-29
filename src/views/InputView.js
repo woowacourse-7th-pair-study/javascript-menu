925d142bfe416b21a35cb4e044b2bcd7cc2eeaa0
sooyeoniya <tndus1306403@naver.com>
2024-11-29 17:07:46 +0900

    refactor(InputView): 공통 로직 분리

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,22 +1,21 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const readPipe = (promptMessage) => {
+  try {
+    return Console.readLineAsync(promptMessage);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
 const InputView = {
-  readCoachesName() {
-    try {
-      return Console.readLineAsync('\n코치의 이름을 입력해 주세요. (, 로 구분)\n');
-    } catch (error) {
-      throw new Error(error.message);
-    }
+  async readCoachesName() {
+    return await readPipe('\n코치의 이름을 입력해 주세요. (, 로 구분)\n');
   },
 
-  readMenuCannotEat(name) {
-    try {
-      return Console.readLineAsync(`\n${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`);
-    } catch (error) {
-      throw new Error(error.message);
-    }
+  async readMenuCannotEat(name) {
+    return await readPipe(`\n${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`);
   },
-
 }
 
 export default InputView;
