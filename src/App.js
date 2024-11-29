23ff905bce4f94b0a43213aa79e384733bc56b9f
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2024-11-29 14:21:25 +0900

    feat: 존재하지 않는 메뉴를 작성하였을 경우 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Controller from './Controller/Controller.js';
 
-const SAMPLE = {
+export const SAMPLE = {
   일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
   한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
   중식: '깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채',
--- src/validator/validateCantEatMenu.js
+++ src/validator/validateCantEatMenu.js
@@ -1,7 +1,16 @@
 import { ERROR_MESSAGE } from '../constant/error.js';
+import { SAMPLE } from '../App.js';
 import { throwWoowaError } from '../util/error.js';
 
 export const validateCantEatMenu = (menus) => {
   if (menus.length < 0 || menus.length > 2)
     throwWoowaError(ERROR_MESSAGE.invalidCantEatMenuLength);
+
+  menus.forEach((menu) => {
+    const allMenu = Object.values(SAMPLE).reduce(
+      (acc, cur) => acc.concat(cur.split(',').map((item) => item.trim())),
+      [],
+    );
+    if (!allMenu.includes(menu)) throwWoowaError(ERROR_MESSAGE.invalidMenuName);
+  });
 };
